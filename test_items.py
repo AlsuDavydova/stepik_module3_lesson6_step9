03db8c3249fe0c5a8da91d48a94048c46d6355d1
AlsuDavydova <ziyatdinova8@yandex.ru>
2022-05-07 18:48:15 +0300

    added files

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_should_be_add_to_basket_button(browser):
+    browser.get(link)
+    #time.sleep(30)
+    assert browser.find_element_by_css_selector("button[value]"),'basket button not found'
