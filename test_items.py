d259024afc2f95a747a2b150191eb1d3700df651
AlsuDavydova <104736320+AlsuDavydova@users.noreply.github.com>
2022-05-09 23:21:10 +0300

    Update test_items.py
    метод find_elements вместо find_element - вместо NoSuchElementException будет AssertionError: basket button not found

--- test_items.py
+++ test_items.py
@@ -5,4 +5,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_should_be_add_to_basket_button(browser):
     browser.get(link)
     #time.sleep(30)
-    assert browser.find_element_by_css_selector("button[value]"),'basket button not found'
+    assert browser.find_elements_by_css_selector("button[value]"),'basket button not found'
